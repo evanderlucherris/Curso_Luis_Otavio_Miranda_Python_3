a51cec2851b6f60136b15ce4cdccc49f75e207a6
evanderlucherris <evanderlucherris@gmail.com>
2025-11-18 17:59:44 -0300

    New lessons

---
+++ aula20_programming_exercise_with_if_and_comparison.py
@@ -0,0 +1,15 @@
+#EXERCÍCIO DE PROGRAMAÇÃO COM IF E COMPARAÇÃO
+
+primeiro_valor = input('Digite o peirmeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'O primeiro valor {primeiro_valor}, '
+        f'é maior ou igual ao segundo valor {segundo_valor}.'
+        )
+else:
+    print(
+        f'O segundo valor {segundo_valor}, '
+        f'é maior que o primeiro valor {primeiro_valor}.'
+        )
