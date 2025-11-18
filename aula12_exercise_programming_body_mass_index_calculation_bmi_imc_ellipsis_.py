f1d1934d1dd53c5c86cee2aabb8d6d845bc52cd9
evanderlucherris <evanderlucherris@gmail.com>
2025-11-18 16:30:14 -0300

    New lessons

---
+++ aula12_exercise_programming_body_mass_index_calculation_bmi_imc_ellipsis_.py
@@ -0,0 +1,13 @@
+nome = 'Evander Lucherris'
+altura = 1.78
+peso = 113
+imc = peso / altura ** 2
+#imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
+
+#Evander Lucherris, tem 1.78m de altura,
+#pesa 113 quilos e seu IMC é
+#35.66468880191895
--- aula15_using_input_function_to_collect_user_data.py
+++ aula15_using_input_function_to_collect_user_data.py
@@ -1,5 +1,5 @@
 # nome = input('Qual o seu nome? ')
-# print(f'O seu nome é {nome}')
+# print(f'O seu nome é {nome=}')
 
 numero_1 = input('Digite um número: ')
 numero_2 = input('Digite outro número: ')
